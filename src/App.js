f99636b0080153f449b1cc18f06db64134fdac4e
hyonheeyoo <hyonheeyoo@hutom.co.kr>
2020-06-23 21:34:38 +0900

    Implement Screen transition effect w/ css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./App.scss";
+
+const Screen = ({ index }) => {
+  return (
+    <TransitionGroup className="screen-transition">
+      <CSSTransition key={index} timeout={300} classNames="show">
+        <div className={index ? `screen screen-${index}` : "screen"} />
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  window.addEventListener("scroll", () => {
+    const { scrollHeight } = document.body;
+    const { innerHeight } = window;
+    const { scrollTop } = document.documentElement;
+    const maxScrollTop = scrollHeight - innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.ceil((scrollFraction * 100) / 25);
+    setIndex(frameIndex);
+  });
+
+  useEffect(() => () => window.removeEventListener("scroll"), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Screen index={index} />
+        <div className="mask">
+          <div className="frame" />
+        </div>
+      </div>
     </div>
   );
 }
